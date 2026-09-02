1fa462085bbb3bb29a56f07c53eae7bc0f9b5b9a
mtack81 <mtack81@live.com>
2026-09-02 18:55:26 -0400

    made adjustments to assignment

---
+++ lab3a.cpp
@@ -0,0 +1,15 @@
+//Program Lab3 A
+#include <stdio.h>
+int main()
+{
+	printf("Here is line %d" ,1);
+	printf("\nHere is line %d" ,2);
+	printf("\nLine %d that shows PI=%.5f with 5 places to the right of the decimal" ,3,3.141592);
+	printf("\nThis $%.2f is correct with only 2 places to the right of the decimal" ,34.6789);
+	printf("\nNext we will demonstrate how my name and initials are shown\n");
+	printf("\n%s %c. %s", "Mike", 'J', "Tackett\n");
+	printf("This is the Output of program Lb 101-3 Include this line!!! Really-No Joke");
+	getchar();
+	return 0;
+}
+
