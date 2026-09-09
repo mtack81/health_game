dc7bb068f8e459158a2345e91a11c7e93cedef0c
mtack81 <mtack81@live.com>
2026-09-09 15:11:02 -0400

    lined stuff up

--- health_game.cpp
+++ health_game.cpp
@@ -8,9 +8,14 @@ int playerxp = 0;
 int steps = 0;
 int level = 1;
 int xpneeded = 1000;
-cout << "Welcome to the world of health! Your current level is:" << level << endl;
-cout << "Let's see how well you did today! Please enter the number of steps you took." << endl;
+cout << "                        Welcome to the world of health!"<< endl;
+cout<< "                              Your current level is:" << level << endl;
+cout<< endl;
+cout << "                         Let's see how well you did today!"<< endl;
+cout<< "                       Please enter the number of steps you took. ";
+
 cin >> steps;
+cout<< endl;
 playerxp = playerxp + (steps/10);
 if (playerxp >= 0 && playerxp <= 999) { level = 1; xpneeded = 1000; }
 if (playerxp >= 1000 && playerxp <= 2199) { level = 2; xpneeded = 2200; }
@@ -22,7 +27,7 @@ if (playerxp >= 9000 && playerxp <= 11199) { level = 7; xpneeded = 12000; }
 if (playerxp >= 12000 && playerxp <= 13599) { level = 8; xpneeded = 13600; }
 if (playerxp >= 13600 && playerxp <= 16199) { level = 9; xpneeded = 16200; }
 if (playerxp >= 16200) { level = 10; xpneeded = 999999; }
-cout <<" Nice Job! You have earned:" << (steps/10) <<  "xp";
+cout <<"                          Nice Job! You have earned:" << (steps/10) <<  "xp";
 
 return 0;
 
